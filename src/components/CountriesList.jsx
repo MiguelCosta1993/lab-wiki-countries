baa1f3f8cb48c7393afd33f56440b4d829171743
Miguel Costa <miguelcosta1993@gmail.com>
2020-08-20 14:43:10 +0100

    done

---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import CountryDetail from './CountryDetail';
+
+class CountriesList extends Component {
+  render() {
+    const countries = this.props.countries;
+    return (
+      <section className="wiki-countries">
+        <ul>
+          {countries.map((country) => {
+            return (
+              <Link to={`/${country.name.official}`} key={country.flag}>
+                {country.name.common} <br />
+              </Link>
+            );
+          })}
+        </ul>
+        <CountryDetail country={this.props.location.pathname} {...this.props} />
+      </section>
+    );
+  }
+}
+
+export default CountriesList;
